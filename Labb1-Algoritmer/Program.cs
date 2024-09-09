a784f1fa26474c176b4134112a3cc16be686e730
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-09-09 14:47:47 +0200

    Fixed an issue when same number occured more than once in the input string. Also some refactoring.

--- Labb1-Algoritmer/Program.cs
+++ Labb1-Algoritmer/Program.cs
@@ -1,42 +1,11 @@
-﻿Console.WriteLine("Vänligen mata in en text:");
+﻿Console.WriteLine("Please enter a text:");
 var input = Console.ReadLine();
 Console.WriteLine("".PadLeft(input.Length, '*'));
-var numbersAsStrings = GetNumberStrings(input);
-PrintInputWithNumbersHighlighted(input, numbersAsStrings);
-Console.WriteLine();
-Console.WriteLine(GetTotalSum(numbersAsStrings));
+PrintNumberStrings(input);
+Console.WriteLine("".PadLeft(input.Length, '*'));
+Console.WriteLine($"Total sum: {GetTotalSumFromListOfNumberStrings(ExtractNumberStringsToList(input))}");
 
-static void PrintInputWithNumbersHighlighted(string input,  List<string> numbersAsStrings)
-{
-    for (int i = 0; i < numbersAsStrings.Count; i++)
-    {
-        int firstIndex = input.IndexOf(numbersAsStrings[i]);
-        int lastIndex = input.IndexOf(numbersAsStrings[i]) + numbersAsStrings[i].Length - 1;
-        
-        for (int j = 0; j < input.Length; j++)
-        {
-            
-            if (j == firstIndex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(input[j]);
-            }
-            else if (j == lastIndex)
-            {
-                Console.Write(input[j]);
-                Console.ForegroundColor = ConsoleColor.Gray;
-            }
-            else
-            {
-                Console.Write(input[j]);
-            }
-        }
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine();
-    }
-}
-
-static List<string> GetNumberStrings(string input)
+static List<string> ExtractNumberStringsToList(string input)
 {
     List<string> numbers = new();
 
@@ -70,14 +39,42 @@ static List<string> GetNumberStrings(string input)
     return numbers;
 }
 
-static long GetTotalSum(List<string> numbersAsStrings)
+static long GetTotalSumFromListOfNumberStrings(List<string> numbersAsStrings)
 {
     long sum = 0;
-    foreach (var item in numbersAsStrings)
+    foreach (var numberString in numbersAsStrings)
     {
-        var asInteger = long.Parse(item);
+        var asInteger = long.Parse(numberString);
         sum += asInteger;
         
     }
     return sum;
+}
+static void PrintNumberStrings(string input)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        bool isInteger = Char.IsDigit(input[i]);
+        if (isInteger)
+        {
+            for (int j = i; j < input.Length; j++)
+            {
+                isInteger = Char.IsDigit(input[j]);
+                if (!isInteger)
+                {
+                    break;
+                }
+                else if (j != i && input[j] == input[i])
+                {
+                    Console.Write(input[0..i]);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(input[i..(j + 1)]);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(input[j..(input.Length - 1)]);
+                    Console.WriteLine();
+                    break;
+                }
+            }
+        }
+    }
 }
