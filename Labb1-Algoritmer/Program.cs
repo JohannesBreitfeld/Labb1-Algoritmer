d782b122c5f0ceee635d7caa0d74f3578c2508d6
JohannesBreitfeld <jbvbreitfeld@gmail.com>
2024-09-09 15:22:14 +0200

    Update Program.cs - refactoring

--- Labb1-Algoritmer/Program.cs
+++ Labb1-Algoritmer/Program.cs
@@ -1,55 +1,10 @@
-﻿Console.WriteLine("Please enter a text:");
+Console.WriteLine("Please enter a text:");
 var input = Console.ReadLine();
 Console.WriteLine("".PadLeft(input.Length, '*'));
 PrintNumberStrings(input);
 Console.WriteLine("".PadLeft(input.Length, '*'));
-Console.WriteLine($"Total sum: {GetTotalSumFromListOfNumberStrings(ExtractNumberStringsToList(input))}");
+Console.WriteLine($"Total sum: {GetTotalSumOfNumberStrings(input)}");
 
-static List<string> ExtractNumberStringsToList(string input)
-{
-    List<string> numbers = new();
-
-    for (int i = 0; i < input.Length; i++)
-    {
-        bool isInteger = Char.IsDigit(input[i]);
-        var numbersString = "";
-
-        if (isInteger)
-        {
-            for (int j = i; j < input.Length; j++)
-            {
-                isInteger = Char.IsDigit(input[j]);
-                if (!isInteger)
-                {
-                    break;
-                }
-                else if (j != i && input[j] == input[i])
-                {
-                    numbersString += input[j];
-                    numbers.Add(numbersString);
-                    break;
-                }
-                else
-                {
-                    numbersString += input[j];
-                }
-            }
-        }
-    }
-    return numbers;
-}
-
-static long GetTotalSumFromListOfNumberStrings(List<string> numbersAsStrings)
-{
-    long sum = 0;
-    foreach (var numberString in numbersAsStrings)
-    {
-        var asInteger = long.Parse(numberString);
-        sum += asInteger;
-        
-    }
-    return sum;
-}
 static void PrintNumberStrings(string input)
 {
     for (int i = 0; i < input.Length; i++)
@@ -77,4 +32,38 @@ static void PrintNumberStrings(string input)
             }
         }
     }
+}
+static long GetTotalSumOfNumberStrings(string input)
+{
+    long sum = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        bool isInteger = Char.IsDigit(input[i]);
+        var numberString = "";
+
+        if (isInteger)
+        {
+            for (int j = i; j < input.Length; j++)
+            {
+                isInteger = Char.IsDigit(input[j]);
+                if (!isInteger)
+                {
+                    break;
+                }
+                else if (j != i && input[j] == input[i])
+                {
+                    numberString += input[j];
+                    long numbersAsInt = long.Parse(numberString);
+                    sum += numbersAsInt;
+                    break;
+                }
+                else
+                {
+                    numberString += input[j];
+                }
+            }
+        }
+    }
+    return sum;
 }
